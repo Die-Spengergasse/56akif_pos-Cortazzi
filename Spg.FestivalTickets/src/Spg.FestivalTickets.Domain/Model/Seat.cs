65425e78ebe5840acaf751d3cd49dd9dc25988e3
Cortazzi <frederick.cortazzi@gmail.com>
2022-10-20 21:56:32 +0200

    WebShop

---
+++ Spg.FestivalTickets/src/Spg.FestivalTickets.Domain/Model/Seat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spg.FestivalTickets.Domain.Model
+{
+    public enum Categories { A, B, C, D, E, F }
+    public class Seat
+    {
+        public int SeatNumber { get; set; }
+        public Event Events { get; set; } = default!;
+        public Categories Category { get; set; }
+    }
+}
