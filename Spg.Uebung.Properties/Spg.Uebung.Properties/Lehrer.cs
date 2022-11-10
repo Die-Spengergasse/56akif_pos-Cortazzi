4b482773a6c46baf13d11b06041e4cd9906aa320
Cortazzi <frederick.cortazzi@gmail.com>
2022-11-10 16:01:28 +0100

    Uebung Properties

---
+++ Spg.Uebung.Properties/Spg.Uebung.Properties/Lehrer.cs
@@ -0,0 +1,48 @@
+﻿namespace Spg.Uebung.Properties
+{
+    public class Lehrer
+    {
+        public string Vorname { get; set; } = string.Empty;
+        public string? Zuname { get; set; } = null;
+        public decimal? Bruttogehalt
+        {
+            get
+            {
+                return _bruttogehalt;
+            }
+            set
+            {
+                if (_bruttogehalt is null)
+                {
+                    _bruttogehalt = value;
+                }
+            }
+
+        }
+        private decimal? _bruttogehalt;
+        public decimal Nettogehalt
+        {
+            get
+            {
+                if (Bruttogehalt is not null)
+                    return (decimal)(_bruttogehalt * 0.8M);
+                else
+                    return 0;
+            }
+        }
+       
+        public string Kuerzel
+        {
+            get 
+            {
+                if (Zuname?.Length >= 3)
+                { 
+                return Zuname?.Substring(0, 3)?.ToUpper() ?? "--";
+                }
+                return "";
+            }
+
+            
+        }
+    }
+}
---
+++ Spg.Uebung.Properties/Spg.Uebung.Properties/Rechteck.cs
@@ -0,0 +1,43 @@
+﻿namespace Spg.Uebung.Properties
+{
+    partial class Program
+    {
+        public class Rechteck
+        {
+            public int Laenge
+            {
+                get
+                {
+                    return _laenge;
+                }
+                set
+                {
+                    if (value < 0)
+                    {
+                        throw new ArgumentException("Ungueltige Laenge!");
+                    }
+                    _laenge = value;
+                }
+            }
+
+            private int _laenge;
+            public int Breite
+            {
+                get
+                {
+                    return _breite;
+                }
+                set
+                {
+                    if (value < 0)
+                    {
+                        throw new ArgumentException("Ungueltige Breite!");
+                    }
+                    _breite = value;
+                }
+            }
+            private int _breite;
+            public int Flaeche { get { return Laenge * Breite; } }
+        }
+    }
+}
